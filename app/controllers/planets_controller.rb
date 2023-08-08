ad45aaa809aeff21bd4aa8942220c74d35e3858d
GeralmyUrena <geralmyuri11@gmail.com>
2023-08-08 19:21:22 -0400

    broken?

--- app/controllers/planets_controller.rb
+++ app/controllers/planets_controller.rb
@@ -7,4 +7,11 @@ class PlanetsController < ApplicationController
 
     render json: planets
   end
+  
 end
+# routes to #index
+# routes to #show (FAILED - 1)
+# routes to #create (FAILED - 2)
+# routes to #update via PUT (FAILED - 3)
+# routes to #update via PATCH (FAILED - 4)
+# routes to #destroy (FAILED - 5)
